101c47ac76d51ed65b1a03616e12c3567736be00
Aatish <aatishda1@gmail.com>
2024-04-01 02:06:28 +0100

    Initialized Sequence Generation

--- DNA_Sequences/V1/DNA_Sequence_Generator.py
+++ DNA_Sequences/V1/DNA_Sequence_Generator.py
@@ -1,1 +1,15 @@
-#Ollo
+import random
+
+def sequence_generator(length):
+    """
+    Generates a random DNA sequence of a specified length.
+    
+    Parameters:
+    - length (int): The length of the DNA sequence to generate.
+    
+    Returns:
+    - str: A random DNA sequence of the specified length.
+    """
+    nucleotides = ['A', 'T', 'C', 'G']
+    sequence = ''.join(random.choice(nucleotides) for _ in range(length))
+    return sequence
